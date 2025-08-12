cfe83db08cb9a1767afc6bd9da6927b33f911e72
Felipe Alves <felipealves1484@gmail.com>
2025-08-12 15:02:45 -0300

    :sparkles: feat: Adiocionado Componentes Hero, Hero CSS e default CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import './assets/Styles/default.css'
+import Hero from './assets/Components/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Hero/>
     </>
   )
 }
