e231c8cb64f06c509d357f801654c1d4942e9d00
Felipe Alves <felipealves1484@gmail.com>
2025-08-12 18:38:18 -0300

    Remove: import React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './assets/Styles/Default.css'
 import Hero from './assets/Components/Hero/Hero'
 import Message from './assets/Components/Message/Message'
