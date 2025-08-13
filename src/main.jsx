38c3ed550ef9bde09da09ebbef77dce0f2945036
Felipe Alves <felipealves1484@gmail.com>
2025-08-13 02:42:04 -0300

    :books docs: Renomenado arquivo para maiusculo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
-import './assets/Styles/default.css'
+import './assets/Styles/Default.css'
 
 import App from './App.jsx'
 import Nik from './Routers/Nik/Nik.jsx'
